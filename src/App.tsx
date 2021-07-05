5cfe69a0891aaf0bc638db158b6dbf09073ecd46
watson <watson@okmaster.com>
2021-07-05 12:53:04 +0800

    feature/ adjust main content area

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import styles from "./App.module.css";
 import { Header, Footer } from "./components";
+import { Row, Col } from "antd";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      {/* detail Content */}
+      <div className={styles["page-content"]}>
+        <Row>
+          <Col span={6} style={{ background: "blue" }}>
+            左側
+          </Col>
+          <Col span={18} style={{ background: "red" }}>
+            右側
+          </Col>
+        </Row>
+      </div>
       <Footer />
     </div>
   );
